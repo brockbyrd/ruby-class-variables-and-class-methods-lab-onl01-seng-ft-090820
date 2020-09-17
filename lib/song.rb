3bcbc79fee5946364543bda23ff850585c85d117
Brock Byrd <brockbyrdd@gmail.com>
2020-09-17 14:48:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,28 +1,16 @@
 class Song
+  @@count
+  @@artists
+  @@genres
 
   attr_accessor :name, :artist, :genre
 
-  @@count = []
-  @@artists = []
-  @@genres = []
 
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
-  end
-
-  def self.count
-    @@count
-  end
-
-  def self.artists
-    @@artists
-  end
-
-  def self.genres
-    @@genres
   end
 
 end
